ca8f7e6e3d4690dc8ed25f6f77ce5172ac64f483
Chris <87771733+ChrisCh4padia@users.noreply.github.com>
2024-06-22 00:37:02 +0200

    add input and display functions to view

--- view.py
+++ view.py
@@ -3,6 +3,23 @@ class GameView:
     def __init__(self):
         pass
 
+
+    @staticmethod
+    def display_message(message):
+        # Display a message to the user
+        pass
+        """
+        Display a message to the user.
+        """
+        print(message)
+
+    @staticmethod
+    def get_user_input(prompt):
+        """
+        Get user input from the console.
+        """
+        return input(prompt)
+
     def print_menu(self):
         self.clear_console()
         print(""" 
